aa7b6bddd0a82265d497439f0d7c83bac0be7fc7
SUBHASISH <subhasishbiswas15@gmail.com>
2021-06-28 00:28:54 +0530

    Model Added

--- EXPENSE-TRACKER-WEB/UI/ExpenseTracker-UI/src/app/cards/cards.module.ts
+++ EXPENSE-TRACKER-WEB/UI/ExpenseTracker-UI/src/app/cards/cards.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddCardComponent } from './add-card/add-card.component';
 import { DisplayCardsComponent } from './display-cards/display-cards.component';
-
-
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AddCardComponent,
-    DisplayCardsComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [AddCardComponent, DisplayCardsComponent],
+  imports: [CommonModule, RouterModule],
 })
-export class CardsModule { }
+export class CardsModule {}
